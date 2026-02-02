4a85e538aaf427c5fc2d7779fd97f72272bafcfe
Hemjotkaur <hemjotkaur786@gmail.com>
2026-02-02 23:32:28 +0530

    feat: employee dashboard

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email,setEmail]= useState();
+  const [password,setPassword] = useState();
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("Form submitted")
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="border-2 border-emerald-600 p-20 rounded-xl">
+        <form 
+        onSubmit={(e)=> {
+          submitHandler(e)
+        }}
+        action=""
+        className="flex flex-col items-center justify-center">
+          <input
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          required
+            className="py-3 text-xl outline-none bg-transparent placeholder:text-gray-400  px-5 border-2 border-emerald-600 rounded-full"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+            className="py-3 mt-3 text-xl outline-none bg-transparent placeholder:text-gray-400  px-5 border-2 border-emerald-600 rounded-full"
+            type="password"
+            placeholder="Enter password"
+          />
+          <button className="py-2 mt-7  border-none  text-xl text-white outline-none  placeholder:text-white  px-28 border-2 bg-emerald-600 rounded-full">Log in</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/other/TaskListNumbers.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TaskListNumbers = ( ) => {
+  return (
+    <div className="flex justify-between gap-5 screen mt-10 ">
+        <div className="h-40 w-[45%] bg-red-400 rounded-xl py-6 px-9">
+            <h2 className="text-3xl font-semibold">0</h2>
+            <h3 classNAme="text-xl font-medium">New Task</h3>
+        </div>
+         <div className="h-40 w-[45%] bg-blue-400 rounded-xl py-6 px-9">
+            <h2 className="text-3xl font-semibold">0</h2>
+            <h3 classNAme="text-xl font-medium">New Task</h3>
+        </div>
+         <div className="h-40 w-[45%] bg-green-400 rounded-xl py-6 px-9">
+            <h2 className="text-3xl font-semibold">0</h2>
+            <h3 classNAme="text-xl font-medium">New Task</h3>
+        </div>
+         <div className="h-40 w-[45%] bg-pink-400 rounded-xl py-6 px-9">
+            <h2 className="text-3xl font-semibold">0</h2>
+            <h3 classNAme="text-xl font-medium">New Task</h3>
+        </div>
+    </div>
+    );
+};
+
+export default TaskListNumbers;
